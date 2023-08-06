380b77cd472656f4ec2e4f1c426f0dccc6666811
Yure Martins <120994376+yuremartins@users.noreply.github.com>
2023-08-06 20:52:47 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,24 +1,17 @@
 from consulta_fipe import consultar_valor_veiculo
 
-def calcular_rentabilidade(valor_compra, taxa_depreciacao, taxa_lucro, prazo_locacao, valor_locacao):
-    valor_venda = valor_compra * (1 - taxa_depreciacao)
-    valor_lucro = valor_compra * taxa_lucro
-    
-    valor_faturamento_periodo = (valor_venda - valor_compra) / prazo_locacao + valor_lucro
-    valor_faturamento_total = valor_faturamento_periodo * prazo_locacao
-    
-    rental_rate = (valor_locacao / valor_compra) * 100
-    
-    return {
-        'valor_compra': valor_compra,
-        'valor_venda': valor_venda,
-        'valor_lucro': valor_lucro,
-        'valor_faturamento_periodo': valor_faturamento_periodo,
-        'valor_faturamento_total': valor_faturamento_total,
-        'rental_rate': round(rental_rate, 2)
-    }
+def calcular_valor_locacao(valor_compra, taxa_depreciacao_desejada, taxa_lucro_desejada, prazo_locacao_desejado):
+    # Cálculo do valor de venda considerando a depreciação
+    valor_venda = valor_compra - (valor_compra * taxa_depreciacao_desejada)
 
-# Exemplo de uso
+    # Cálculo do valor de locação necessário para atingir a taxa de lucro desejada
+    valor_locacao_necessario = valor_venda / ((1 + taxa_lucro_desejada) ** prazo_locacao_desejado)
+
+    # Cálculo do rental rate (porcentagem do valor de locação em relação ao valor de compra)
+    rental_rate = (valor_locacao_necessario / valor_compra) * 100
+
+    return round(valor_locacao_necessario, 2), round(valor_venda, 2), round(rental_rate, 2)
+
 data = {
     "codigoTabelaReferencia": 231,
     "codigoTipoVeiculo": 1,
@@ -32,13 +25,15 @@ data = {
 
 valor_veiculo = consultar_valor_veiculo(data)
 valor_compra = float(valor_veiculo["Valor"].replace("R$", "").replace(".", "").replace(",", "."))
-valor_locacao = 3000  # Valor de locação (exemplo)
 
-# Valores para cálculo de rentabilidade
-taxa_depreciacao = 0.6
-taxa_lucro = 0.5
+# Valores desejados para cálculo de rentabilidade
+taxa_depreciacao = 0.4
+taxa_lucro = 0.2
 prazo_locacao = 12
 
-resultado = calcular_rentabilidade(valor_compra, taxa_depreciacao, taxa_lucro, prazo_locacao, valor_locacao)
-print(resultado)
+valor_locacao, valor_venda, rental_rate = calcular_valor_locacao(valor_compra, taxa_depreciacao, taxa_lucro, prazo_locacao)
+
+print("Valor de Locação: R$ {:.2f}".format(valor_locacao))
+print("Valor de Venda: R$ {:.2f}".format(valor_venda))
+print("Rental Rate: {:.2f}%".format(rental_rate))
 
