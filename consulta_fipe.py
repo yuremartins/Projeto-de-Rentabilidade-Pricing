c25bc0aa591dcc9aa512b8e9abd93c9dc94cdd08
Yure Martins <120994376+yuremartins@users.noreply.github.com>
2023-08-06 13:15:26 -0300

    Create consulta_fipe.py

---
+++ consulta_fipe.py
@@ -0,0 +1,21 @@
+from fipe_api import fipe_json
+
+def consultar_valor_veiculo(data):
+    url = "ConsultarValorComTodosParametros"
+    return fipe_json(url, data)
+
+# Exemplo de uso
+data = {
+    "codigoTabelaReferencia": 231,
+    "codigoTipoVeiculo": 1,
+    "codigoMarca": 26,
+    "codigoModelo": 4925,
+    "ano": "2010-1",
+    "codigoTipoCombustivel": 1,
+    "anoModelo": 2011,
+    "tipoConsulta": "tradicional"
+}
+
+valor_veiculo = consultar_valor_veiculo(data)
+print(valor_veiculo)
+
