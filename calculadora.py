dfb9fe9da3200d4e66ea4ee663982a9bf9df5187
Yure Martins <comercialyure@gmail.com>
2023-08-07 12:25:37 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -14,12 +14,12 @@ def calcular_valor_locacao(valor_compra, taxa_depreciacao_desejada, taxa_lucro_d
     valor_venda = valor_compra - (valor_compra * taxa_depreciacao_atualizada)
 
     # Cálculo do valor de locação necessário para atingir a taxa de lucro desejada
-    valor_locacao_necessario = ((valor_venda) - (valor_compra * taxa_lucro_desejada)) / prazo_locacao_desejado
+    valor_locacao_necessario = ((valor_compra + (valor_compra * taxa_lucro_desejada)) - valor_venda) / prazo_locacao_desejado
 
     # Cálculo do rental rate (porcentagem do valor de locação em relação ao valor de compra)
     rental_rate = (valor_locacao_necessario / valor_compra) * 100
 
-    faturamento_total = (valor_locacao_necessario * prazo_locacao_desejado + valor_venda) - valor_compra
+    faturamento_total = (valor_locacao_necessario * prazo_locacao_desejado) + valor_venda
 
     return round(valor_locacao_necessario, 2), round(valor_venda, 2), round(rental_rate, 2), round(faturamento_total,2), round(taxa_depreciacao_atualizada,2), round(valor_depreciado,2)
 
